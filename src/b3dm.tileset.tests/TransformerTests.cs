cc406e06137ae00312658fb643f0b58af39994a5
Bert Temme <btemme@hotmail.com>
2020-03-18 10:55:53 +0100

    adding rotateX + translate functions

---
+++ src/b3dm.tileset.tests/TransformerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using Wkx;
+
+namespace B3dm.Tileset.Tests
+{
+    public class TransformerTests
+    {
+        [Test]
+        public void TestTranslate()
+        {
+            // arrange
+            var point = new Point(1, 2, 3);
+
+            // act
+            var pointTranslated = point.Translate(10, 20, 30);
+
+            // assert
+            Assert.IsTrue(pointTranslated.Equals(new Point(11, 22, 33)));
+        }
+
+        [Test]
+        public void TestRotateX()
+        {
+            // arrange
+            var point = new Point(1, 2.5, 3);
+            var pointRotatedXExpected = new Point(1, -2.5, -3);
+
+            // act
+            var pointRotatedXActual = point.RotateX(Math.PI);
+
+            // assert
+            Assert.IsTrue(pointRotatedXActual.Equals(pointRotatedXExpected));
+        }
+    }
+}
---
+++ src/b3dm.tileset/extensions/PointExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Wkx;
+
+namespace B3dm.Tileset
+{
+    public static class PointExtensions
+    {
+        public static Point Translate(this Point p, double dx, double dy, double dz)
+        {
+            return new Point((double)p.X + dx, (double)p.Y + dy, (double)p.Z + dz);
+        }
+
+        public static Point RotateX(this Point p, double angleRadians)
+        {
+            // formula from: https://cartosig.webs.upv.es/jaspa/v0.2.0/manual/html/ST_RotateX.html
+            var x = p.X;
+            var y = Math.Cos(angleRadians) * p.Y - Math.Sin(angleRadians) * p.Z;
+            var z = Math.Sin(angleRadians) * p.Y + Math.Cos(angleRadians) * p.Z;
+            return new Point((double)x, Math.Round((double)y,5) ,Math.Round((double)z,5));
+        }
+
+        public static double[] ToVector(this Point p)
+        {
+            var vector = new double[] { (double)p.X, (double)p.Y, (double)p.Z };
+
+            return vector;
+        }
+
+    }
+}
