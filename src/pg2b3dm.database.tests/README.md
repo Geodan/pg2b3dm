526e3cc3e639e5d3e2fc3aee08d5e0d639083e81
Bert Temme <btemme@hotmail.com>
2022-07-26 14:05:26 +0200

    add first database test

---
+++ src/pg2b3dm.database.tests/README.md
@@ -0,0 +1,15 @@
+﻿# Database tests
+
+To run these tests, run the Docker database with test table first.
+
+Build:
+
+```
+$ docker build -t delaware .
+```
+
+Run:
+
+```
+$ docker run -e POSTGRES_PASSWORD=postgres -p 5432:5432 delaware
+```
---
+++ src/pg2b3dm.database.tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using B3dm.Tileset;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace pg2b3dm.database.tests
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void FirstTest()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            var conn = new NpgsqlConnection(config["DB_CONNECTION_STRING"]);
+            var sql = "select count(*) from delaware_buildings";
+            var records = DatabaseReader.ReadScalar(conn, sql);
+            Assert.IsTrue(records == 22532);
+        }
+    }
+}
