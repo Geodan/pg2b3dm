13aeac6696741de9697d321c2257d98def84bcca
Bert Temme <btemme@hotmail.com>
2022-10-04 10:29:55 +0200

    updating packages

--- src/b3dm.tileset.tests/BoundingBoxTests.cs
+++ src/b3dm.tileset.tests/BoundingBoxTests.cs
@@ -10,10 +10,12 @@ namespace B3dm.Tileset.Tests
         [Test]
         public void FirstTEst()
         {
+
+            var bbox = new BoundingBox(5, 51, 6, 52);
             var bbdenhelder = new BoundingBox(4.709058, 52.945690, 4.790726, 52.973842);
             // var bb = new BoundingBox(4.9777, 52.1021, 5.1859, 52.0682);
             //var bb_dela = new BoundingBox(-75.6145, 39.0964, -75.4353, 39.2124);
-            var radians = bbdenhelder.ToRadians();
+            var radians = bbox.ToRadians();
 
             var s= $"{radians.XMin}, {radians.YMin}, {radians.XMax}, {radians.YMax}";
         }
