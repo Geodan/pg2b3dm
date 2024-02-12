7ab810ece4d709c078150d7f4b4b4af10d71d8cb
Bert Temme <btemme@hotmail.com>
2024-02-12 15:00:37 +0100

    attempt_3dtiles_using_wgs84

---
+++ src/b3dm.tileset/Ellipsoid1.cs
@@ -0,0 +1,16 @@
+﻿
+namespace B3dm.Tileset;
+public class Ellipsoid1
+{
+    public Ellipsoid1()
+    {
+        SemiMajorAxis = 6378137;
+        SemiMinorAxis = 6356752.3142478326;
+        Eccentricity = 0.081819190837553915;
+    }
+    public double SemiMajorAxis { get; }
+    public double SemiMinorAxis { get; }
+
+    public double Eccentricity { get; }
+
+}
--- src/wkb2gltf.core/SpatialConverter.cs
+++ src/wkb2gltf.core/SpatialConverter.cs
@@ -5,9 +5,37 @@ namespace Wkb2Gltf;
 
 public static class SpatialConverter
 {
+    public static Matrix4x4 EcefToEnu(Vector3 position)
+    {
+        var east = new Vector3();
+        east.X = -position.Y;
+        east.Y = position.X;
+        east.Z = 0;
+
+        var eastNormalize = Vector3.Normalize(east);
+
+        var normalUp = GetNormalUp(position);
+        var upNormalize = Vector3.Normalize(normalUp);
+        var north = Vector3.Cross(normalUp, east);
+        var northNormalized = Vector3.Normalize(north);
+
+        return Matrix.GetMatrix(position, eastNormalize, northNormalized, upNormalize);
+    }
+    private static Vector3 GetNormalUp(Vector3 position)
+    {
+        var ellipsoid = new Ellipsoid1();
+        var x = 1.0 / (ellipsoid.SemiMajorAxis * ellipsoid.SemiMajorAxis);
+        var y = 1.0 / (ellipsoid.SemiMajorAxis * ellipsoid.SemiMajorAxis);
+        var z = 1.0 / (ellipsoid.SemiMinorAxis * ellipsoid.SemiMinorAxis);
+
+        var oneOverRadiiSquared = new Vector3((float)x, (float)y, (float)z);
+        var res = Vector3.Multiply(position, oneOverRadiiSquared);
+        return Vector3.Normalize(res);
+    }
+
     public static Vector3 GeodeticToEcef(double lon, double lat, double alt)
     {
-        var ellipsoid = new Ellipsoid();
+        var ellipsoid = new Ellipsoid1();
         var equatorialRadius = ellipsoid.SemiMajorAxis;
         var eccentricity = ellipsoid.Eccentricity;
         var latitudeRadians = ToRadius(lat);
